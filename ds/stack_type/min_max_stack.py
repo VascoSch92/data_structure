79077993922548dbdfe663192c8aa851fa16405d
vasco <vasco.schiavo@protonmail.com>
2024-09-13 17:20:18 +0200

    update min max stack

--- ds/stack_type/min_max_stack.py
+++ ds/stack_type/min_max_stack.py
@@ -1,5 +1,4 @@
-from typing import Sequence, Optional, Any
-
+from typing import Sequence, Optional, Any, Tuple
 from ds import Stack
 
 __all__ = ["MinMaxStack"]
@@ -13,13 +12,28 @@ class MinMaxStack:
             )
         return super().__new__(cls)
 
-    def __init__(self) -> None:
-        self._stack = Stack()
-        self._range = Stack()
-        self._len = 0
+    def __init__(self, _from: Optional[Sequence] = None) -> None:
+        self._stack, self._range = (
+            self._instantiate_from_sequence(_from) if _from else (Stack(), Stack())
+        )
+        self._len = len(self._stack) if _from else 0
+
+    @staticmethod
+    def _instantiate_from_sequence(source: Sequence) -> Tuple[Stack, Stack]:
+        """Private method to instantiate a stack from a sequence."""
+        source = source[::-1]
+        stack, range = Stack(), Stack()
+        for idx, value in enumerate(source):
+            stack.push(value)
+            range.push((min(source[: idx + 1]), max(source[: idx + 1])))
+
+        return stack, range
 
     def __len__(self) -> int:
         return self._len
+
+    def __bool__(self) -> bool:
+        return self._len > 0
 
     def __repr__(self) -> str:
         return self._stack.__repr__()
@@ -51,3 +65,11 @@ class MinMaxStack:
 
     def max(self) -> Any:
         return self._range.peek()[1]
+
+
+if __name__ == "__main__":
+    stack = MinMaxStack([1, 2, 3, 4, 5])
+    while stack:
+        print(stack)
+        print(stack.peek(), stack.min(), stack.max())
+        stack.pop()
--- tests/test_stack_type/test_min_max_stack.py
+++ tests/test_stack_type/test_min_max_stack.py
@@ -5,6 +5,10 @@ def test_initialize_stack():
     _ = MinMaxStack()
 
 
+def test_initialize_stack_from_sequence():
+    _ = MinMaxStack([1, 2, 3, 4, 5])
+
+
 def test_push():
     stack = MinMaxStack()
     for j in range(1, 11):
